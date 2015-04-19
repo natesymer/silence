5808e3e59f4fb964604a22306ab7a62956903eda
Nathaniel Symer <nate@natesymer.com>
2015-04-19 13:58:06 -0400

    implemented if

--- Felony.hs
+++ Felony.hs
@@ -8,17 +8,12 @@ import           Control.Concurrent
 import           System.IO.Unsafe
 import           Text.ParserCombinators.Parsec as Parsec hiding (spaces)
 
-data Procedure = Procedure {
-  environment :: Environment,
-  parameterNames :: [String],
-  name :: String
-}
-
 data Expression = Atom String
                 | String String
                 | Integer Integer
                 | Real Double
                 | Bool Bool
+                | Procedure Environment [String] [Expression]
                 | Null
                 | Cell Expression Expression deriving (Show)
 
@@ -32,8 +27,8 @@ type EnvironmentState = Monad.State.State Environment
 -------------------------------------------------------------------------------
 -- | Environment
 
-setEnvironment :: String -> Expression -> Environment -> Environment
-setEnvironment key expr (Environment bindings_ parent_) = Environment (M.insert key expr bindings_) parent_
+setEnvironment :: String -> Environment -> Expression -> Environment
+setEnvironment key (Environment bindings_ parent_) expr = Environment (M.insert key expr bindings_) parent_
 
 getEnvironment :: String -> Environment -> Maybe Expression
 getEnvironment key (Environment bindings_ parent_) = case M.lookup key bindings_ of
@@ -41,6 +36,16 @@ getEnvironment key (Environment bindings_ parent_) = case M.lookup key bindings_
                                                        Nothing -> case parent_ of
                                                                     Nothing -> Nothing
                                                                     Just p -> getEnvironment key p
+                                                                    
+zipEnvironment :: [String] -> [Expression] -> Environment
+zipEnvironment keys exprs = Environment (fromList $ zip keys exprs) Nothing
+
+extendEnvironment :: Environment -> Environment -> Environment
+extendEnvironment parent (Environment bindings Nothing) = Environment bindings $ Just parent
+extendEnvironment _ _ = lispError "Cannot extend environment with an environment that already has a parent."
+
+childEnvironment :: Environment -> [String] -> [Expression] -> Environment
+childEnvironment env keys values = extendEnvironment env $ zipEnvironment keys values
 
 emptyEnvironment :: Environment
 emptyEnvironment = Environment M.empty Nothing
@@ -100,6 +105,15 @@ parseExpr = parseAtom <|> parseString <|> parseInteger <|> parseReal <|> parseQu
 -------------------------------------------------------------------------------
 -- | Lisp core
 
+-- print a lisp expression as-is to the console
+lispShow :: Expression -> String
+lispShow (Atom x) = x
+lispShow (String x) = "\"" ++ x ++ "\""
+lispShow (Integer x) = show x
+lispShow (Real x) = show x
+lispShow Null = "'()"
+lispShow (Cell a b) = "(" ++ (lispShow a) ++ " " ++ (lispShow b) ++ ")"
+
 -- reads lisp code into an AST
 lispRead :: String -> Expression
 lispRead input = case parse parseExpr "lisp" input of
@@ -107,34 +121,55 @@ lispRead input = case parse parseExpr "lisp" input of
     Right val -> val
 
 -- evaluates lisp expressions
-lispEval :: Expression -> Expression
-lispEval expr = evalState (lispEval' expr) emptyEnvironment
-
-lispEval' :: Expression -> EnvironmentState Expression
-lispEval' expr = do
-  env <- get
+lispEval :: Environment -> Expression -> (Expression, Environment)
+lispEval env expr = do
   case expr of
     (Atom a) -> case getEnvironment a env of
-                  Just lkup -> return $ lispEval $ cons lkup $ cdr expr
-                  Nothing -> lispError "Atom not found in environment."
-    (Integer a) -> return (Integer a)
-    (String a) -> return (String a)
-    (Real a) -> return (Real a)
-    (Bool a) -> return (Bool a)
-    (Cell (Atom "define") e) -> return $ car e -- TODO: modify env
-    -- (Cell "begin" e) ->
-    -- (Cell "fork" e) ->
-    -- (Cell "lambda" e) ->
-    -- (Cell a e) -> -- TODO: Look up in primitive and in the env
-    e -> do
-      return e
-      
+                    Just lkup -> lkup
+                    Nothing -> lispError "Atom not found in environment."
+    (Integer a) -> ((Integer a), env)
+    (String a) -> ((String a), env)
+    (Real a) -> ((Real a), env)
+    (Bool a) -> ((Bool a), env)
+    (Cell (Atom "+") e) -> case car e of
+                             Real value = (value + (lispEval (Cell (Atom "+") (cdr e))), env)
+                             Integer value = (value + (lispEval (Cell (Atom "+") (cdr e))), env)
+                             _ -> lispError "Cannot add non-number value"
+    (Cell (Atom "*") e) -> case car e of
+                             Real value = value * (lispEval (Cell (Atom "*") (cdr e)))
+                             Integer value = value * (lispEval (Cell (Atom "*") (cdr e)))
+                             _ -> lispError "Cannot add non-number value"
+    (Cell (Atom "if") (Cell (Bool False) (Cell _ (Cell iffalse _)))) -> lispEval isfalse env
+    (Cell (Atom "if") (Cell (Bool True) (Cell iftrue _))) -> lispEval iftrue env
+    (Cell (Atom "if") e) -> lispError "Invalid special form: if."
+    (Cell (Atom "cons") (Cell a (Cell b Null))) = Cell a b
+    (Cell (Atom "cons") (Cell a (Cell b _))) = lispError "cons: Too many arguments."
+    (Cell (Atom "cons") a) = lispError "cons: Too few arguments."
+    (Cell (Atom "car") (Cell a _)) = (a, env)
+    (Cell (Atom "car") e) = lispError "car: Cannot take the car of: " ++ (show e)
+    (Cell (Atom "cdr") (Cell _ b)) = (b, env)
+    (Cell (Atom "cdr") e) = lispError "car: Cannot take the cdr of: " ++ (lispShow (Cell (Atom "display") (Cell (Atom "quote") e)))
+    (Cell (Atom "display") e) = seq (unsafePerformIO $ putStrLn $ lispShow e) Null
+    (Cell (Atom "bind") (Cell (Atom key) value)) -> (Atom key, setEnvironment key env value)
+    (Cell (Atom "begin") e) -> (lispBegin env e, env)
+    (Cell (Atom "fork") e) -> seq (unsafePerformIO $ forkOS $ lispBegin env a) (Null, env)
+    (Cell (Atom "lambda") (Cell argNamesList bodyList)) -> (Procedure env (fromConsList argNamesList) (fromConsList bodyList), env)
+    (Cell (Atom "lambda") e) -> lispError "Invalid lambda: " ++ (lispShow e)
+    (Cell (Procedure procenv argNamesList bodies) e) = (lispBegin (childEnvironment env (map (\(Atom key) -> key) (fromConsList argNamesList)) (fromConsList e)) (toConsList bodies), env)
+    (Cell a e) -> case getEnvironment a env of
+                    Just lkup -> return $ lispEvalEnv (cons lkup e) env
+                    Nothing -> lispError "Atom not found in environment."
+    e -> lispError $ "Invalid form: " ++ lispShow e
+    
 repl :: String -> IO ()
 repl prompt = do
   putStrLn prompt
   input <- getLine
-  putStrLn $ show $ lispEval $ lispRead input
+  putStrLn $ show $ lispBegin emptyEnvironment $ lispRead input
   repl prompt
+  
+evalProgram :: String -> String
+evalProgram code = lispBegin emptyEnvironment $ Cell (Atom "begin") $ lispRead code
   
 -------------------------------------------------------------------------------
 -- | Lisp primitive functions
@@ -160,6 +195,10 @@ toConsList :: [Expression] -> Expression
 toConsList (x:xs) = foldl append (cons x Null) xs
 toConsList [] = Null
 
+fromConsList :: Expression -> [Expression]
+fromConsList Null = []
+fromConsList (Cell a b) = a:fromConsList b
+
 -- returns true if the expression is a cons list
 isConsList :: Expression -> Bool
 isConsList Null = True
@@ -171,34 +210,18 @@ lispError :: [Char] -> t
 lispError a = error a
 
 -- evals each expression in a Lisp list and returns the result of the last eval
-lispBegin :: Expression -> Expression
-lispBegin (Cell a Null) = lispEval a
-lispBegin (Cell a b) = seq (lispEval a) (lispBegin b)
-lispBegin a = lispEval a
-
--- evals the first/only argument on a new native OS thread, not waiting for 
--- the thread to exit
--- lispFork :: Expression -> ThreadId
--- lispFork a = unsafePerformIO $ forkOS $ do
---   (lispBegin a)
---   yield
-
--- evals the first/only argument on a new native OS thread, waiting for it to
--- finish
-lispForkWait :: Expression -> Expression
-lispForkWait a = unsafePerformIO $ do
-    m <- newEmptyMVar
-    forkOS $ putMVar m $ lispBegin a
-    r <- takeMVar m
-    return r
+lispBegin :: Environment -> Expression -> (Expression, Environment)
+lispBegin env (Cell a Null) = lispEval env a
+lispBegin env (Cell a b) = lispBegin (snd $ lispEval env a) b
+lispBegin env a = lispEval env a
 
 -- (apply + 1 2 3 4 5)
--- lispApply :: Expression -> Expression -> Expression -> Expression
--- lispApply (Atom exprName)
+lispApply :: Expression -> Expression -> Expression -> Expression
+lispApply (Atom exprName)
 
 -------------------------------------------------------------------------------
 -- | Main (temporary)
   
 main = do
   let code = "(this)"
-  print $ lispRead code
+  print $ lispEval $ lispRead code
