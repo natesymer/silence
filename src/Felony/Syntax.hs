c0a673a5ffc4218a707d197f899d8bd0eab18605
Nathaniel Symer <nate@natesymer.com>
2016-02-22 11:52:57 -0500

    Improved baseN parser & factor of 2 speed improvement for atoms

--- src/Felony/Syntax.hs
+++ src/Felony/Syntax.hs
@@ -22,11 +22,13 @@ parseFelony :: ByteString -> [Expression]
 parseFelony = either (error . show) id . parse code ""
 
 code :: Parsec ByteString () [Expression]
-code = skipw *> endBy expr skipw-- many $ skipw *> expr <* skipw
-  where skipw = try $ skipMany $ void whitespace <|> void comment
+code = many $ ignored *> expr <* ignored
   
 expr :: Parsec ByteString () Expression
 expr = choice [equoted,elist,enumber,eatom,estring,ebool]
+
+ignored :: Parsec ByteString () ()
+ignored = try $ skipMany $ void whitespace <|> void comment
 
 whitespace :: Parsec ByteString () Char
 whitespace = (space <|> tab <|> newline <|> crlf) <?> "whitespace"
@@ -38,10 +40,10 @@ lineEnd :: Parsec ByteString () ()
 lineEnd = void newline <|> void crlf <|> eof
 
 eatom :: Parsec ByteString () Expression
-eatom = fmap (Atom . B.pack . map toLower) ident <?> "atom"
+eatom = fmap (Atom . B.pack) ident <?> "atom"
   where ident = ((:) <$> initial <*> many subseq) <|> (pure <$> exc)
-        initial = letter <|> symbol
-        subseq = initial <|> digit <|> exc
+        initial = toLower <$> (letter <|> symbol)
+        subseq = toLower <$> (initial <|> digit <|> exc)
         exc = oneOf ".+-"
         symbol = oneOf "?!$%&*/|:<=>~_^"
 
@@ -77,25 +79,26 @@ equoted = char '\'' *> (wrap <$> expr) <?> "quote"
 
 elist :: Parsec ByteString () Expression
 elist = char '(' *> list' id <* char ')' <?> "list"
-  where skipw = skipMany (void whitespace <|> void comment)
-        list' acc = option (acc Null) $ do
-          a <- skipw *> expr <* skipw
-          choice [
-            char '.' *> skipw *> (acc . Cell a <$> expr), -- dotted
-            list' $ acc . Cell a] -- proper
+  where list' acc = option (acc Null) $ do
+          a <- ignored *> expr <* ignored
+          choice [char '.' *> ignored *> (acc . Cell a <$> expr), -- dotted
+                  list' $ acc . Cell a] -- proper
 
 enumber :: Parsec ByteString () Expression
-enumber = choice [real,dec,hex,binary,octal,ntal] <?> "number"
+enumber = choice [real,dec,nonBaseTen] <?> "number"
   where real = try $ real' <$> (option 0 dec') <*> (char '.' *> (withLength $ baseN 10))
         real' x (base,y) = real'' (fromInteger x) (fromInteger y) base
         real'' x y = Real . (+) x . (/) y . (^) 10.0
         dec'   = (*) <$> sign <*> baseN 10
         dec    = try $ fmap Integer dec'
-        hex    = try $ string "#x" *> (Integer <$> baseN 16)
-        binary = try $ string "#b" *> (Integer <$> baseN 2)
-        octal  = try $ string "#o" *> (Integer <$> baseN 8)
-        ntal   = try $ char '#' *> baseN 10 <* char '|' >>= f >>= fmap Integer . baseN
-          where f n = if n <= 36 then return n else fail "unsupported base greater than 36"
+        nonBaseTen = char '#' *> choice [
+          char 'x' *> (Integer <$> baseN 16),
+          char 'b' *> (Integer <$> baseN 2),
+          char 'o' *> (Integer <$> baseN 8),
+          baseN 10 <* char '|' >>= ntalPred >>= fmap Integer . baseN]
+        ntalPred n
+          | n <= 36 = return $ fromInteger n
+          | otherwise = fail "bases greater than 36 are unsupported"
         sign   = option 1 $ char '-' *> return (-1)
         getCol = sourceColumn <$> getPosition
         withLength p = do
@@ -104,19 +107,16 @@ enumber = choice [real,dec,hex,binary,octal,ntal] <?> "number"
           c' <- getCol
           return ((c'-c),p')
 
--- TODO: ensure doesn't parse chars that don't make sense given @n@.
--- eg: @baseN 10@ on @"12a"@ shouldn't parse the @'a'@.
-baseN :: Integer -> Parsec ByteString () Integer
+baseN :: Int -> Parsec ByteString () Integer
 baseN n = many1 alphaNum >>= f 0
   where f acc [] = return acc
         f acc (x:xs)
-          | dec < 10 = g dec
-          | hxu < 36 = g hxu
-          | hxl < 36 = g hxl
-          | otherwise = unexpected "non-numeric digit"
-          where dec = fromIntegral $ ord x - ord '0'
-                hxl = fromIntegral $ ord x - ord 'a' + 10
-                hxu = fromIntegral $ ord x - ord 'A' + 10
-                g y
-                  | y >= n = fail "digit is larger than the base"
-                  | otherwise = f ((+) acc . (*) y . (^) n . length $ xs) xs
+          | dec   < 10 = if dec   < n then g dec   else err
+          | alphu < 36 = if alphu < n then g alphu else err
+          | alphl < 36 = if alphl < n then g alphl else err
+          | otherwise = err
+          where dec = ord x - ord '0'
+                alphl = ord x - ord 'a' + 10
+                alphu = ord x - ord 'A' + 10
+                err = fail $ show x ++ " is larger than base " ++ show n ++ "."
+                g y = f ((+) acc . toInteger . (*) y . (^) n . length $ xs) xs
